f6b32f4296124fc4faaf5deec15f15399f69a2c4
Carl Olson <43393940+Hunter71a@users.noreply.github.com>
2019-06-09 20:31:07 -0400

    Add project files.

---
+++ StoreFront/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StoreFront.Models;
+
+namespace StoreFront.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "RacerXX14: Chief Technologist";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "RacerXX14 Computer Solutions, LLC";
+
+            return View();
+        }
+
+        public IActionResult Custom()
+        {
+            
+
+            var valueSeries = new Custom
+            {
+                CPU = "Intel 9500K",
+                GPU = "Nvidia 1660ti",
+                Case = "Corsiar Carbide SPEC-01",
+                Cooling = "Cooler Master Big Air"
+            };
+
+            return View("Custom", valueSeries);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ StoreFront/Models/Custom.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoreFront.Models
+{
+    public class Custom
+    {
+        [Editable(false)]
+        [Display(Name = "Central Processing Unit")]
+        public string CPU { get; set; }
+        [Editable(false)]
+        [Display(Name = "Graphics Card")]
+        public string GPU { get; set; }
+        [Editable(false)]
+        [Display(Name = "Case Model")]
+        public string Case { get; set; }
+        [Editable(false)]
+        [Display(Name = "Cooling Option")]
+        public string Cooling { get; set; }
+    }
+}
